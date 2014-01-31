b95ffb7e74588e0e697dc44fc3056706d069480d
Lyle Troxell <lyle@troxell.com>
2014-01-30 21:10:55 -0800

    added mark and some vids

--- build/js/app.js
+++ build/js/app.js
@@ -2,39 +2,34 @@
 
 // If using JSLint for syntax debugging, include the following two lines for looser management
 /*jslint browser: true, sloppy: true, white: true*/
-/*global $, console, alert, app*/
+/*global $, console, app*/
 
-$(function() { 
+$(function() {
 	app.init();
 });
 
-var app = { 
-
-	init: function() { 
+var app = {
+	init: function() {
 		//this.listeners();
 		console.log(this.settings.name + "(v" + this.settings.version + ") Started");
 		this.randomList($('#speakerList'), 'section');
 		this.randomList($('#mainSponsors'),'li');
 		this.randomList($('#speakerSponsors'),'li');
 	},
-	
 	randomList: function(target, itemSel) {
 		var daList = target.children(itemSel);
 		if (daList) {
 			daList.sort(function(a,b){
-	            return parseInt( Math.random()*10 ) > 5 ? 1 : -1;
+				return parseInt( Math.random()*10 ) > 5 ? 1 : -1;
 			}).prependTo(target);
 		}
 	},
-
-	listeners: function() { 
+	listeners: function() {
 		// Application Listeners can be loaded here for easy configuration
 	},
-	
-	settings: { 	
-		name: "JS.everywhere(2013)",	
-		version: "1.0.0",
+	settings: {
+		name: "JS.everywhere(2013)",
+		version: "1.1.0",
 		ga_analytics: "UA-30228440-1"
 	}
-	
 };
